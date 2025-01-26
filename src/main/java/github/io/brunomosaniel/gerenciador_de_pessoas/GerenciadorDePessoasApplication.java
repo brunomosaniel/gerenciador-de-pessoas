c87feb899f70ec3df3f2c603eced2676cc21f77c
Bruno Mosaniel <brunomosaniel.dev@gmail.com>
2025-01-26 00:17:48 -0300

    Initial commit

---
+++ src/main/java/github/io/brunomosaniel/gerenciador_de_pessoas/GerenciadorDePessoasApplication.java
@@ -0,0 +1,13 @@
+package github.io.brunomosaniel.gerenciador_de_pessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDePessoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDePessoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/io/brunomosaniel/gerenciador_de_pessoas/GerenciadorDePessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package github.io.brunomosaniel.gerenciador_de_pessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDePessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
